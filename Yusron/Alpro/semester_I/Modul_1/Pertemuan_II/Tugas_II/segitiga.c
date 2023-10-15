65e3766fede3fef9a6fd181c644768e70f2269c6
Arbi2997 <111668538+Arbi2997@users.noreply.github.com>
2023-10-16 03:16:52 +0700

    Add files via upload

---
+++ Yusron/Alpro/semester_I/Modul_1/Pertemuan_II/Tugas_II/segitiga.c
@@ -0,0 +1,19 @@
+/*
+    Program: Assigment1.c
+    Name: Muhamad Yusron Noval
+    NIM: 2350081003
+    Class: A
+    Description: ...
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    // Kamus
+    int a,b,c;
+
+    // Algoritma
+
+    return 0;
+}
