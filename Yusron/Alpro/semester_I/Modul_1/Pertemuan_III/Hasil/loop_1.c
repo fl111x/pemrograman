56405b02b0b4f34cc67f580078f1d0380ace01a2
Arbi2997 <111668538+Arbi2997@users.noreply.github.com>
2023-10-25 02:14:00 +0700

    Add files via upload

--- Yusron/Alpro/semester_I/Modul_1/Pertemuan_III/Hasil/loop_1.c
+++ Yusron/Alpro/semester_I/Modul_1/Pertemuan_III/Hasil/loop_1.c
@@ -1,8 +1,7 @@
 /*
     Program: loop_1.c
-    Name: Muhamad Yusron Noval
-    NIM: 2350081003
-    Class: A
+    Author: 2350081003/Muhamad Yusron Noval
+    Date: 24/10/2023
     Description: Perulangan menggunakan for loop
 */
 #include <stdio.h>
