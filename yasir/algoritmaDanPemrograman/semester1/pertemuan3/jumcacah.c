d6130b4d3f9413cb5cc1d0c1f2105be9953143e3
Yasir Noval <yanval.deyy@gmail.com>
2023-10-24 17:21:07 +0700

    jumcacah.c berhasil

--- yasir/algoritmaDanPemrograman/semester1/pertemuan3/jumcacah.c
+++ yasir/algoritmaDanPemrograman/semester1/pertemuan3/jumcacah.c
@@ -7,52 +7,30 @@
  */
 
 #include <stdio.h>
-//program jumlah bilangan cacah
 
-int main()
-{
-    //kamus
+int main() {
+    // KAMUS
+    int N, data,i,a,hasil,X;
 
-    int N,p[50],i,a,hasil,X;
-
-    //algoritma
-
-    up:
-
-    printf("\t=======================================");
-    printf("\n\t| PROGRAM PENJUMLAHAN BILANGAN CACAH |\n");
-    printf("\t=======================================");
-    printf("\nBY : HARFIN A.F - 3411211006\n\n");
-    printf("input pencacah : ");scanf("%d",&N);
-    printf("\n=======================================\n");
+    // ALGORITMA
+    printf("input pencacah : ");
+    scanf("%d",&N);
 
     i = 1;
     hasil = 0;
     // inisialisasi
     do{
         //dilakukan aksi 1x terlebih dahulu
-        printf("input data : "); scanf("%d",&p[i]);printf("");
+        printf("input data : ");
+        scanf("%d",&data);printf("");
         if (i<N){
             printf("_____________+\n");
         }
-        hasil = hasil + p[i];
+        hasil = hasil + data;
         i++;
     }while (i<=N);
-    printf("\n================================");
+    printf("\n\n================================");
 
     printf("\nBanyak Data Integer\t\t : %d", N);
     printf("\nNilai Penjumlahan\t\t : %d ", hasil);
-    printf("\nNilai yang terdapat di dalam Larik : ");
-    for (i=1;i<=N;i++){
-        printf("%d",p[i]);
-        if (i<N)
-            printf("	,	");
-    }printf("\n================================");
-    printf("Masukan Salah satu Input Dibawah !\n1.> Refresh Program \n2.> KELUAR->\n");
-    printf("jawaban anda : ");scanf("%d",&X);
-    if(X==2){
-        return 0;
-    }else{
-        system ("cls"); printf("\nInput Salah Masukan Lagi \n\n\n");goto up;
-    }
 }
