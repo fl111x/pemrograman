9d63dda0e3008bb5aa4d6794e73e8af83e62ae72
Muhamad Yasir Noval <myasirnoval@gmail.com>
2023-10-29 05:37:33 +0700

    pertemuan 4

--- yasir/algoritmaDanPemrograman/semester1/pertemuan3/jumcacah.c
+++ yasir/algoritmaDanPemrograman/semester1/pertemuan3/jumcacah.c
@@ -10,27 +10,30 @@
 
 int main() {
     // KAMUS
-    int N, data,i,a,hasil,X;
+    int n, data, i, hasil;
 
     // ALGORITMA
     printf("input pencacah : ");
-    scanf("%d",&N);
+    scanf("%d", &n);
 
     i = 1;
     hasil = 0;
     // inisialisasi
-    do{
+    do {
         //dilakukan aksi 1x terlebih dahulu
         printf("input data : ");
-        scanf("%d",&data);printf("");
-        if (i<N){
+        scanf("%d", &data);
+        printf("");
+        if (i < n) {
             printf("_____________+\n");
         }
         hasil = hasil + data;
         i++;
-    }while (i<=N);
+    } while (i <= n);
     printf("\n\n================================");
 
-    printf("\nBanyak Data Integer\t\t : %d", N);
+    printf("\nBanyak Data Integer\t\t : %d", n);
     printf("\nNilai Penjumlahan\t\t : %d ", hasil);
+
+    return 0;
 }
