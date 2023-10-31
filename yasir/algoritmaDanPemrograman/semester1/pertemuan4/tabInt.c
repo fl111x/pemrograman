a9cedef716d13374340d8038f18af900aea951fa
Yasir Noval <yanval.deyy@gmail.com>
2023-10-31 14:02:49 +0700

    tugas pertemuan 4

---
+++ yasir/algoritmaDanPemrograman/semester1/pertemuan4/tabInt.c
@@ -0,0 +1,44 @@
+/**
+ * Program: tabInt.c
+ * Author: 2350081004/Muhamad Yasir Noval
+ * Date: ...............
+ * Description: .............
+ */
+
+#include <stdio.h>
+#include <conio.h>
+#include "boolean.h"
+
+#define nMax 10
+
+typedef struct {
+    int T[nMax + 1];
+    int neff;
+}TabInt;
+
+int main() {
+    // KAMUS
+    TabInt MyTab;
+    int N;
+    int i;
+
+    // ALGORITMA
+    MyTab.neff = 0;
+    printf("Masukan bilangan: ");
+    scanf("%d", &N);
+
+    while (N != 999) {
+        if (MyTab.neff < nMax) {
+            MyTab.neff++;
+            MyTab.T[MyTab.neff] = N;
+        }
+
+        printf("Masukan bilangan: ");
+        scanf("%d", &N);
+    }
+
+    // cetak tabel
+    for (i = 1; i <= MyTab.neff; i++) {
+        printf("<%d>", MyTab.T[i]);
+    }
+}
